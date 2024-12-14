8e9235488f8d6700246b6e3e3cbaa418f6104710
Phùng Khánh <pun.hnahk@gmail.com>
2024-12-14 19:34:16 +0700

    123

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import posthog from "posthog-js";
 import React from "react";
 import UrlShortener from "./components/UrlShortener";
+
+posthog.init("phc_tDi1S6OTLf6Plys9GtibFFzcXniz0Sx1mqrBRQUXUrH", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+});
 
 function App() {
   return (
